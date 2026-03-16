c15e865a43abb9d7f51b2eb6145eb54e8b7280d0
elkarpeto <daffa1mubarok@gmail.com>
2026-03-16 21:05:16 +0700

    feat: menambahkan 2 metode, namun untuk faktorisasi masih ada ERROR pada bagian TODO

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import streamlit as st
-from method import BiSection, FalsePosition, FixedPoint, NewtonRaphson, Secant
+from method import (
+    BiSection,
+    FalsePosition,
+    FixedPoint,
+    NewtonRaphson,
+    Secant,
+    MNewtonRaphson,
+    PolynomFactorization,
+)
 
 
 st.title("Komputasi Numerik Informatika ITS 📐")
 
 metode = st.selectbox(
     "Pilih Metode",
-    ["Bi Section", "False Position", "Fixed Point", "Newton Raphson", "Secant"],
+    [
+        "Bi Section",
+        "False Position",
+        "Fixed Point",
+        "Newton Raphson",
+        "Secant",
+        "Modified Newton Raphson",
+        "Polynomial Factorization",
+    ],
 )
 
 st.subheader("Buat Fungsi")
@@ -55,24 +71,38 @@ else:
 
     st.info(f"Fungsi terbentuk: {fungsi}")
 
-x_true = st.number_input("Nilai true", value=24.0)
 max_iter = st.number_input("Maksimum Iterasi", value=10)
-tol = st.number_input("Toleransi (%)", value=0.1)
 
 if metode in ["Bi Section", "False Position"]:
+    x_true = st.number_input("Nilai true", value=24.0)
+    tol = st.number_input("Toleransi (%)", value=0.1)
     xl = st.number_input("Batas bawah (xl)", value=18.0)
     xu = st.number_input("Batas atas (xu)", value=37.0)
 
 elif metode == "Fixed Point":
+    x_true = st.number_input("Nilai true", value=24.0)
+    tol = st.number_input("Toleransi (%)", value=0.1)
     x0 = st.number_input("Nilai awal (x0)", value=20.0)
     lambda_val = 0.0001
 
 elif metode == "Newton Raphson":
+    x_true = st.number_input("Nilai true", value=24.0)
+    tol = st.number_input("Toleransi (%)", value=0.1)
     x0 = st.number_input("Nilai awal (x0)", value=20.0)
 
 elif metode == "Secant":
+    x_true = st.number_input("Nilai true", value=24.0)
+    tol = st.number_input("Toleransi (%)", value=0.1)
     x0 = st.number_input("x0", value=18.0)
     x1 = st.number_input("x1", value=37.0)
+
+elif metode == "Modified Newton Raphson":
+    x_true = st.number_input("Nilai true", value=24.0)
+    tol = st.number_input("Toleransi (%)", value=0.1)
+    x0 = st.number_input("Nilai awal (x0)", value=20.0)
+
+elif metode == "Polynomial Factorization":
+    pass
 
 if st.button("Hitung"):
     if metode == "Bi Section":
@@ -95,20 +125,34 @@ if st.button("Hitung"):
         solver = Secant(fungsi, x0, x1, x_true, max_iter, tol)
         df, steps, akar, err = solver.solve()
 
-    st.subheader("Hasil Iterasi")
-    st.dataframe(df, use_container_width=True, hide_index=True)
+    elif metode == "Modified Newton Raphson":
+        solver = MNewtonRaphson(fungsi, x0, x_true, max_iter, tol)
+        df, steps, akar, err = solver.solve()
+
+    elif metode == "Polynomial Factorization":
+        solver = PolynomFactorization(fungsi, max_iter)
+        df, roots, err = solver.solve()
+
+    st.subheader("Hasil Iterasi 📋")
+    st.dataframe(df, width="stretch", hide_index=True)
 
     if err is not None:
         st.warning(err)
 
+    # TODO: untuk polynomial factorization, log terakhir harus dibenerin
+    if steps is not None and metode != "Polynomial Factorization:
+        st.divider()
+        st.subheader("Langkah-Langkah Perhitungan 🕵️‍♂️")
+        for i in range(len(steps)):
+            st.markdown(steps[i])
+            if i != len(steps) - 1:
+                st.space("medium")
+
+    if metode == "Polynomial Factorization":
+        st.success(
+            f"Akar-akar polinomial: {', '.join(str(f"x_{i+1} = {root}") for i, root in enumerate(roots) if not root.isna())}"
+        )
+
     st.divider()
-
-    st.subheader("Langkah-Langkah Perhitungan 🕵️‍♂️")
-    for i in range(len(steps)):
-        st.markdown(steps[i])
-        if i != len(steps) - 1:
-            st.space("medium")
-
-    st.divider()
-
-    st.success(f"Akar pendekatan: {akar}")
+    if err is None:
+        st.success(f"Akar pendekatan: {akar}")
