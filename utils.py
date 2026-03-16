9ec1a803c0c378570f6b2e51d73a29cf6df56bfb
elkarpeto <daffa1mubarok@gmail.com>
2026-03-16 20:59:49 +0700

    fix: fix beberapa edge case pada utils

--- utils.py
+++ utils.py
@@ -7,10 +7,11 @@ def custom_round(num: float) -> float:
         num = float(num)
         if math.isnan(num) or math.isinf(num):
             raise ValueError("Invalid number")
-        temp = float(
+
+        return float(
             Decimal(str(num)).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
         )
-        return temp
+
     except Exception:
         raise ValueError("Invalid number")
 
@@ -18,12 +19,26 @@ def custom_round(num: float) -> float:
 def Et(true: float, approx: float) -> float:
     approx = custom_round(approx)
     true = custom_round(true)
+
+    if true == 0.0 and approx == 0.0:
+        return 0.0
+
+    if true == 0.0:
+        return custom_round(abs(approx) * 100)
+
     return custom_round(abs((true - approx) / true) * 100)
 
 
 def Ea(approx: float, approx_old: float) -> float:
     approx = custom_round(approx)
     approx_old = custom_round(approx_old)
+
+    if approx == 0 and approx_old == 0:
+        return 0.0
+
+    if approx == 0.0:
+        return custom_round(abs(approx_old) * 100)
+
     if approx == 0:
         return float("inf") if approx_old != 0 else 0
     return custom_round(abs((approx - approx_old) / approx) * 100)
